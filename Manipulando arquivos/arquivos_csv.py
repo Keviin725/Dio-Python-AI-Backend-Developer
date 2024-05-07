f6143b0ff2190ff721b47c1a02318c66a900972e
Kevin Machava <kevinmachava20@gmail.com>
2024-05-07 11:58:23 +0200

    Update arquivos_csv.py

--- Manipulando arquivos/arquivos_csv.py
+++ Manipulando arquivos/arquivos_csv.py
@@ -2,9 +2,11 @@ import csv
 from pathlib import Path
 
 BOOT_PATH = Path(__file__).parent
+COLUNA_NOME = 0
+COLUNA_IDADE = 1
 
-try:
-    with open(BOOT_PATH / 'pessoas.csv', 'w', encoding='utf-8') as arquivo:
+'''try:
+    with open(BOOT_PATH / 'pessoas.csv', 'w', newline='', encoding='utf-8') as arquivo:
         escritor = csv.writer(arquivo)
         escritor.writerow(['Nome', 'Idade'])
         escritor.writerow(['Lucas', 26])
@@ -16,10 +18,25 @@ except IOError as execption:
     print(f"Erro de I/O {execption}")
 
 
+ '''   
 try:
-    with open(BOOT_PATH / 'pessoas.csv', 'r', encoding='utf-8') as entrada:
+    with open(BOOT_PATH / 'pessoas.csv', 'r', newline='', encoding='utf-8') as entrada:
         leitor = csv.reader(entrada)
-        for linha in leitor:
-            print(linha)
+        for idx, linha in enumerate(leitor):
+            if idx == 0:
+                continue
+            if len(linha) > max(COLUNA_NOME, COLUNA_IDADE):
+                print(f"Nome: {linha[COLUNA_NOME]}")
+                print(f"Idade: {linha[COLUNA_IDADE]}")
+            else:
+                print("Linha não tem colunas suficientes")
 except IOError as execption:
     print(f"Erro de I/O {execption}")
+'''    
+try:
+    with open(BOOT_PATH / 'pessoas.csv', 'r', newline='', encoding='utf-8') as entrada:
+        leitor = csv.DictReader(entrada)
+        for linha in leitor:
+            print(linha['Nome'], linha['Idade'])
+except IOError as execption:
+    print(f"Erro de I/O {execption}")'''
